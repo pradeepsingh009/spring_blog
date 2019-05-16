fca84968cfa1a6576bc4a3150b42529475731e8e
spradeepsinghsaini@gmail.com <spradeepsinghsaini@gmail.com>
2019-05-17 02:18:15 +0530

    annotations done

--- src/main/java/com/learn/blog/entity/ArticleComment.java
+++ src/main/java/com/learn/blog/entity/ArticleComment.java
@@ -1,5 +1,22 @@
 package com.learn.blog.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="article_comment")
 public class ArticleComment {
 
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="comment")
+	private String comment;
+	
 }
